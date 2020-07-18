97303f95dcd3fa3ffa5b36cc356bb90e15ec35f7
EgimenoS <ergisan@gmail.com>
2020-07-18 17:20:21 +0200

    añadidos svg para background de cards; añadido reset css simple; importada fuente

--- javascript/main.js
+++ javascript/main.js
@@ -1,1 +1,3 @@
 import productList from "./modules/products.js";
+
+const cart = []; // array que contendrá la lista de productos en el carro
---
+++ javascript/modules/products.js
@@ -0,0 +1,29 @@
+const productList = [
+  {
+    name: "platanos",
+    img: "./assets/images/platanos.svg",
+    price: 1.4,
+  },
+  {
+    name: "arroz",
+    img: "./assets/images/arroz.svg",
+    price: 2.1,
+  },
+  {
+    name: "pescado",
+    img: "./assets/images/pescado.svg",
+    price: 7.4,
+  },
+  {
+    name: "tarta",
+    img: "./assets/images/tarta.svg",
+    price: 4.5,
+  },
+  {
+    name: "verdura",
+    img: "./assets/images/verdura.svg",
+    price: 2,
+  },
+];
+
+export default productList;
